eb2475bb02d6fed1dc252cb0f1a339466c95738c
Pepper-Wood <dipippo.k@gmail.com>
2015-03-13 13:44:00 -0400

    Lab3-Compass: End of Day 1
    The main modifications to this code include rearranging blocks of text
    to make it more readable and easier to navigate as well as setting
    temporary variables prior to when we actually wire the board.

--- Lab 3/Steering/Lab3_Steering.c
+++ Lab 3/Steering/Lab3_Steering.c
@@ -1,7 +1,17 @@
-/* Sample code for Lab 3.1. This code provides a basic start. */
+/*	Names: Kathryn DiPippo, Rebecca Halzack, Seth Rutman
+	Section: 4B
+	Date: 4/7/2015
+	File name: Lab 3-1 - Compass
+	Description: Implement the servo or speed controller hardware and write a calibration/test code. The code
+	should allow the user to create different pulsewidths for the servo or speed controller.
+*/
+
+
 #include <c8051_SDCC.h>
 #include <stdio.h>
 #include <stdlib.h>
+
+
 //-----------------------------------------------------------------------------
 // 8051 Initialization Functions
 //-----------------------------------------------------------------------------
@@ -14,9 +24,16 @@ unsigned int PW_CENTER //= _____;
 unsigned int PW_MIN //= _____;
 unsigned int PW_MAX //= _____;
 unsigned int PW = 0;
+
 //-----------------------------------------------------------------------------
-// Main Function
+// Global Variables
 //-----------------------------------------------------------------------------
+__sbit __at 0xA4 BILEDA0;
+
+
+
+
+//=============================================================================
 void main(void)
 {
 	// initialize board
@@ -36,47 +53,8 @@ void main(void)
 	while(1) Steering_Servo();
 	
 }
+
 //-----------------------------------------------------------------------------
-// Port_Init
-//-----------------------------------------------------------------------------
-//
-// Set up ports for input and output
-void Port_Init()
-{
-	//P1MDOUT = ________ ;//set output pin for CEX0 in push-pull mode
-}
-//-----------------------------------------------------------------------------
-// XBR0_Init
-//-----------------------------------------------------------------------------
-//
-// Set up the crossbar
-//
-void XBR0_Init()
-{
-	//XBR0 = __________ ; //configure crossbar as directed in the laboratory
-}
-//-----------------------------------------------------------------------------
-// PCA_Init
-//-----------------------------------------------------------------------------
-//
-// Set up Programmable Counter Array
-//
-void PCA_Init(void)
-{
-// reference to the sample code in Example 4.5 - Pulse Width Modulation
-// implemented using the PCA (Programmable Counter Array), p. 50 in Lab Manual.
-}
-//-----------------------------------------------------------------------------
-// PCA_ISR
-//-----------------------------------------------------------------------------
-//
-// Interrupt Service Routine for Programmable Counter Array Overflow Interrupt
-//
-void PCA_ISR ( void ) __interrupt 9
-{
-// reference to the sample code in Example 4.5 - Pulse Width Modulation
-// implemented using the PCA (Programmable Counter Array), p. 50 in Lab Manual.
-}
 void Steering_Servo()
 {
 char input;
@@ -84,18 +62,57 @@ char input;
 	input = getchar();
 	if(input == 'r') //if 'r' - single character input to increase the pulsewidth
 	{
-		// _________________________
-		// _________________________
+		PW += ; // arbitrary number to increment
 		if(PW < PW_MIN) // check if less than pulsewidth minimum
-		//PW = ________; //set PW to a minimum value
+		{
+			PW = PW_MIN; //set PW to a minimum value
+		}
 	}
 	else if(input == 'l') //if 'l' - single character input to decrease the pulsewidth
 	{
-		// _________________________
-		// _________________________
+		PW -= ; // arbitrary number to decrement
 		if(PW > PW_MAX) // check if pulsewidth maximum exceeded
-			//PW = ________; // set PW to a maximum value
+		{
+			PW = PW_MAX; // set PW to a maximum value
+		}
 	}
 	printf("PW: %u\n", PW);
 	PCA0CP0 = 0xFFFF - PW;
+}
+
+
+//=============================================================================
+//-----------------------------------------------------------------------------
+// Set up ports for input and output
+void Port_Init()
+{
+	//P1MDOUT = ________ ;//set output pin for CEX0 in push-pull mode
+}
+
+
+//-----------------------------------------------------------------------------
+// Set up the crossbar
+void XBR0_Init()
+{
+	XBR0 = 0x27;	//configure crossbar as directed in the laboratory
+}
+
+//-----------------------------------------------------------------------------
+// Set up Programmable Counter Array
+void PCA_Init(void)
+{
+	PCA0CPM0 = 0xC2;	// CCM0 in 16-bit compare mode
+	PCA0CN = 0x40;		// Enable PCA Counter
+	// configure CEX0
+	// configure CEX1 just cause
+	// configure CEX2
+}
+
+
+//-----------------------------------------------------------------------------
+// Interrupt Service Routine for Programmable Counter Array Overflow Interrupt
+void PCA_ISR ( void ) __interrupt 9
+{
+// reference to the sample code in Example 4.5 - Pulse Width Modulation
+// implemented using the PCA (Programmable Counter Array), p. 50 in Lab Manual.
 }
